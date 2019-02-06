4b76d9aaf209b295f6dd9b2f8282668de0bfa5ff
Sultana Parvin <Simy4825@gmail.com>
2019-02-05 20:56:05 -0500

    Added logic for converting fahrenheit to celsius and showing the result using a Toast

--- app/src/main/java/com/simy/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/simy/temperatureconverter/MainActivity.java
@@ -1,7 +1,12 @@
 package com.simy.temperatureconverter;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Show Welcome message
+        Toast WelcomeToast = Toast.makeText(getApplicationContext(),"Welcome to my App",Toast.LENGTH_LONG);
+        WelcomeToast.show();
+
+        //Convert Button
+        final Button convertButton = findViewById(R.id.button_convert);
+
+        //Fahrenheit edit text
+        final EditText fahrenheitEditText = findViewById(R.id.text_temp_fahrenheit);
+
+        //Celsius edit text
+        final EditText celsiusEditText = findViewById(R.id.text_temp_celsius);
+
+
+
+        convertButton.setOnClickListener(new View.OnClickListener(){
+           public void onClick(View v){
+               String fahrenheitValue =  fahrenheitEditText.getText().toString();
+               Float fahrenheitValueFloat = Float.parseFloat(fahrenheitValue);
+               Float result = (fahrenheitValueFloat - 32) * 5/9;
+
+               //String celsiusValue  = celsiusEditText.getText().toString();
+               //int celsiusValueInteger =  Integer.getInteger(celsiusValue);
+               Toast.makeText(getApplicationContext(),"Current Temp in Celsius is "+result.toString(),Toast.LENGTH_LONG).show();
+           }
+        });
     }
 }
